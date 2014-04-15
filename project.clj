d1e5999ef85e9cd85b2e83a4cbfc1c21f0399efa
James Conroy-Finn <james@logi.cl>
2014-04-15 11:28:47 +0100

    Bump version to 0.0.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/constraint-validations-url "0.0.4"
+(defproject listora/constraint-validations-url "0.0.5-SNAPSHOT"
   :description "Adds support for validating URLs to constraint"
   :url "https://github.com/listora/constraint-validations-url"
 
