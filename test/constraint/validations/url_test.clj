6a147d2552457f268c9f8b756e5d194514babd25
Roman Scherer <roman.scherer@burningswell.com>
2014-05-06 18:17:11 +0200

    Add support for basic auth credentials in URLs.
    The stock Apache UrlValidator doesn't support credentials in URLs. The
    authority-pattern is taken from the Apache codebase and prefixed with an
    optional regex for credentials to fix this.

--- test/constraint/validations/url_test.clj
+++ test/constraint/validations/url_test.clj
@@ -5,52 +5,62 @@
             [constraint.validations.url :refer :all]))
 
 (def ^:private valid-http
-  "http://example.com")
+  ["http://:@example.com"
+   "http://:password@example.com"
+   "http://tiger:scotch@example.com"
+   "http://user:@example.com"
+   "http://user@example.com"])
 
 (def ^:private valid-https
-  "https://example.com")
+  (map #(.replace % "http://" "https://") valid-http))
 
-(def ^:private domain-only
-  "example.com")
+(def ^:private invalid
+  ["example.com"
+   "http://:example.com"
+   "http://:example.com"])
 
 (deftest test-with-scheme
   (let [validator (url ["http"])]
-    (is (valid? validator valid-http))
 
-    (is (instance? java.net.URL (coerce validator valid-http))
-        "transforms the URL string into a java.net.URL")
+    (doseq [url valid-http]
+      (is (valid? validator url))
+      (is (instance? java.net.URL (coerce validator url))
+          "transforms the URL string into a java.net.URL"))
 
-    (is (not (valid? validator valid-https)))
-    (is (not (valid? validator domain-only)))
-
-    (is (= (validate validator domain-only)
-           [{:error :invalid-url
-             :message "Expected URL with scheme \"http\""
-             :found domain-only}]))))
+    (doseq [url (concat invalid valid-https)]
+      (is (not (valid? validator url)))
+      (is (= (validate validator url)
+             [{:error :invalid-url
+               :message "Expected URL with scheme \"http\""
+               :found url}])))))
 
 (deftest test-with-schemes
   (let [validator (url ["http" "https"])]
-    (doseq [url [valid-http valid-https]]
+
+    (doseq [url (concat valid-http valid-https)]
       (is (valid? validator url))
       (is (instance? java.net.URL (coerce validator url))
-          "transforms the http URL string into a java.net.URL"))
+          "transforms the URL string into a java.net.URL"))
 
-    (is (not (valid? validator domain-only)))
-
-    (is (= (validate validator domain-only)
-           [{:error :invalid-url
-             :message "Expected URL with scheme \"http\", or \"https\""
-             :found domain-only}]))))
+    (doseq [url invalid]
+      (is (not (valid? validator url)))
+      (is (= (validate validator url)
+             [{:error :invalid-url
+               :message "Expected URL with scheme \"http\", or \"https\""
+               :found url}])))))
 
 (deftest test-coercions
   (let [schemes ["http" "https"]
         validator (url schemes)
-        example-url (URL. "http://example.com")
         coercions (url-coercions schemes)]
-    (is (valid? URL (str example-url) coercions))
-    (is (= (coerce URL (str example-url) coercions) example-url))
 
-    (is (= (validate (url schemes) domain-only)
-           [{:error :invalid-url
-             :message "Expected URL with scheme \"http\", or \"https\""
-             :found domain-only}]))))
+    (doseq [url (concat valid-http valid-https)
+            :let [url (URL. url)]]
+      (is (and (is (valid? URL (str url) coercions))
+               (is (= (coerce URL (str url) coercions) url)))))
+
+    (doseq [url invalid]
+      (is (= (validate validator url)
+             [{:error :invalid-url
+               :message "Expected URL with scheme \"http\", or \"https\""
+               :found url}])))))
