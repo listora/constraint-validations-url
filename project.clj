5ba0ce1f550094caa779dd31feb2d69b6bd47f1d
Roman Scherer <roman.scherer@burningswell.com>
2014-05-20 17:45:44 +0200

    Prepare release 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/constraint-validations-url "0.0.6-SNAPSHOT"
+(defproject listora/constraint-validations-url "0.0.6"
   :description "Adds support for validating URLs to constraint"
   :url "https://github.com/listora/constraint-validations-url"
 
